338758a72db1a74eb70dc82bbafcc087188ae90f
Yaohui Wu <yaohui758178454@163.com>
2015-12-18 11:27:32 +0800

    Support for replication configuration.

--- installMysql.sh
+++ installMysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-usage='用法: （以root用户执行） sudo ./installMysql.sh [/path/to/mysql.tar.gz] [server id(optional)]'
+usage='用法: （以root用户执行） sudo ./installMysql.sh [/path/to/mysql.tar.gz] [server id(optional)] [master ip (optional)]'
 #'server id: set 1 if installing master, other number if installing slaves'
 if [ $# -eq 0 ];then
 	echo $usage
@@ -10,10 +10,18 @@ fi
 include ./tools/platform.sh
 quick_dir=$PWD
 server_id='1'
+master_ip=''
+
+repl_user='repl'
+repl_pass='p4ssword'
+repl_network='192.168.%.%'
 
 echo "mysql tar file :$1"
 if [ $# -gt 1 ] ; then
 	server_id=$2
+fi
+if [ $# -gt 2 ];then
+	master_ip=$3
 fi
 
 _F=`expr match $1 '.*/\(.*.tar.[gx]z\)'`
@@ -102,13 +110,14 @@ if [ ! -e my.cnf ] ; then
 fi
 
 sed -i '/\[mysqld\]/ a\bind-address=0.0.0.0 \
-basedir=/usr/local/soft/mysql/mysql \
 lower_case_table_names=1 	\
 character-set-server=utf8 	\
 collation-server=utf8_general_ci \
-max_connections = 1000 \
-log_bin = mysql-bin \
-server_id = '${server_id}'' my.cnf
+max_connections=1000 \
+log_bin=mysql-bin \
+server_id='${server_id}'' my.cnf
+
+sed -i '/\[mysqld\]/ a\basedir='$mysql_home'' my.cnf
 
 if [ $server_id -ne 1 ];then
 	sed -i '/\[mysqld\]/ a\relay_log=mysql-relay-bin \
@@ -143,8 +152,6 @@ cd $last_dir
 
 sudo /etc/init.d/mysql.server start
 sudo /etc/init.d/mysql.server status
-#service mysql.server start
-#service mysql.server status
 
 echo "Do you want to config security of mysql ? [y/n]"
 read is_ensure_security
@@ -155,15 +162,49 @@ fi
 bin_mysql=bin/mysql
 
 echo "设置root远程访问"
-read -s -p "Enter your root password:" root_password 
+read -s -p "Enter root password:" root_password 
 $bin_mysql -u root -p$root_password -e "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '"$root_password"';flush PRIVILEGES;"
 
-echo "是否创建本地复制帐号?（y/n）"
-read need_repl
-if [ $need_repl = 'y' ];then
-	$bin_mysql -u root -p$root_password -e "GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO repl@'192.168.%.%' IDENTIFIED BY 'p4ssword';FLUSH PRIVILEGES;"
+echo "创建本地复制帐号"
+$bin_mysql -u root -p$root_password -e "GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO ${repl_user}@'${repl_network}' IDENTIFIED BY '${repl_pass}';FLUSH PRIVILEGES;"
+
+if [ $server_id -ne 1 -a  -n "$master_ip" ] ; then
+	echo '配置slave复制'
+	fmstatus='mstatus.txt'
+	$bin_mysql -u root -p$root_password -h $master_ip -s -e "show master status;" | grep "mysql-bin" > $fmstatus
+	log_file=`awk '{print $1}' $fmstatus`
+	log_pos=`awk '{print $2}' $fmstatus`
+
+	echo "master log-> log file:$log_file log pos:$log_pos"
+
+	if [ -n "$log_file" -a -n "$log_pos" ];then
+		change_master_cmd="CHANGE MASTER TO MASTER_HOST='${master_ip}', MASTER_USER='${repl_user}', MASTER_PASSWORD='${repl_pass}', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${log_pos};"
+		$bin_mysql -u root -p$root_password -e "$change_master_cmd"
+		echo "Start slave now?(y/n)"
+		read is_start_now
+		if [ $is_start_now = 'y' ];then
+			echo "开启复制"
+			$bin_mysql -u root -p$root_password -e "start slave;"
+			echo "验证复制"
+			echo "在master: ${master_ip}中创建数据库mydb，表t（包含整型列num）,写入行1"
+			test_db='mydb'
+			test_t='t'
+			$bin_mysql -u root -p$root_password -h $master_ip -e "drop database if exists ${test_db}; create database ${test_db}; use ${test_db}; create table if not exists ${test_t} (num int); insert into ${test_t}(num) values(1);" 
+			sleep 0.1 
+			echo "0.1秒后，在slave中查询"
+			$bin_mysql -u root -p$root_password -e "select * from ${test_db}.${test_t};"
+			echo "清理测试"
+			$bin_mysql -u root -p$root_password -h $master_ip -e "drop database if exists ${test_db};"
+		fi
+	else
+		echo "不能配置slave复制：无法得到日志文件和位置。"
+	fi
+
+	#rm status file finally
+	rm -rf $fmstatus
 fi
 
+ver=`$bin_mysql --version`
+echo "VERSION: $ver"
 echo "安装成功。Mysql安装在$mysql_home 。"
-ver=`bin/mysql --version`
-echo $ver
+
